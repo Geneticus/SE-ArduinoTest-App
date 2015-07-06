21038dd71d6989b627f187b88a2994123bdf3c51
Geneticus <Geneticus0@gmail.com>
2015-07-06 05:49:10 -0700

    CHECKING POINTERS

--- SerialReader/SerialReader.ino
+++ SerialReader/SerialReader.ino
@@ -8,40 +8,40 @@ int led = 13;
 /*-----( Declare Variables )-----*/
 const byte numChars = 32;
 char receivedChars[numChars];
-boolean newData = false;
+boolean message = false;
 
 
 void setup() 
 {
   pinMode(led, OUTPUT);      // sets the digital pin as output
   Serial.begin(115200);  //set serial to 115200 baud rate
-  Serial.println(" Type in Box above, . ");
 }
 
 void loop() 
 {  
-                      //Serial String Format (StartBit)(Priority,Item,CurrentValue,MaxValue,DataType)(StopBit)
+                      //Serial String Format (StartBit)(DataType,Item,CurrentValue,MaxValue)(StopBit)
                       //StartBit = "\x02"
-                      //Priority = 2 digit number starting from 00
-                      //Item = 2 digit number starting from 00
-                      //CurrentValue = string number
-                      //MaxValue = string number
-                      //DataType = 2 Digit Alpha srating from AA
+                      //DataType = Integer value starting from 1. Indicates if values are Int, Float, Bool, etc.                      
+                      //Item = Number begining with 1. Indicates what the value is i.e. Health, speed, dampers.
+                      //CurrentValue = can be any type as indicated by DataType
+                      //MaxValue = can be any numeric as indicated by DataType
                       //StopBit = "\x03"
-	recvWithStartEndMarkers();
-	showNewData();
+	recvData();
+	processMessage();
 }
 
-void recvWithStartEndMarkers() 
+void recvData() 
 {
 	static boolean recvInProgress = false;
 	static byte ndx = 0;
-	char startMarker = '\x02';
-	char endMarker = '\x03';
+	//char startMarker = 0x2;
+	//char endMarker = 0x3;
+        char startMarker = '<';
+	char endMarker = '>';
 	char rc;
 	
 	// if (Serial.available() > 0) {
-        while (Serial.available() > 0 && newData == false) {
+        while (Serial.available() > 0 && message == false) {
 		rc = Serial.read();
 
 		if (recvInProgress == true) {
@@ -56,25 +56,60 @@ void recvWithStartEndMarkers()
 				receivedChars[ndx] = '\0'; // terminate the string
 				recvInProgress = false;
 				ndx = 0;
-				newData = true;
+				message = true;
 			}
 		}
 
-		if (rc == startMarker) {
+		else if (rc == startMarker) {
 			recvInProgress = true;
 		}
 	}
 }
 
-void showNewData() {
-	if (newData == true) 
+void processMessage() 
+{
+          char delimiter[]= ",";
+          char* valPosition; 
+          int integerarray[] = {0};
+          int dataType = 0;
+          int Item = 0;
+          int CurrentValueInt = 0;
+          float CurrentValueFloat = 0;
+          bool CurrentValueBool = 0;
+          
+          int i=0;
+          
+          
+          
+	if (message == true) 
         {
+		            Serial.println(receivedChars);
+                //This initializes strtok with our string to tokenize
+                valPosition = strtok(receivedChars, delimiter);
+                 if(valPosition[0] = 1)
+                 {
+                  dataType = atoi(valPosition);
+                  Serial.println(dataType);
+                  Serial.println("test01"); 
+                 }
+                 while(valPosition != NULL)
+                    {                    
+                    integerarray[i] = atoi(valPosition);
+                    valPosition = strtok(NULL, delimiter);
+                    i++;                    
+                    }
+                    String serialDebug = "";
+                    serialDebug = " Array of ";
+                    serialDebug +=  i,DEC ;
+                    serialDebug += " integers created.";
+                    Serial.println(serialDebug);                   
+
+                
+		message = false;
+    //Message received:
                 digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-                delay(2000);               // wait for 2 seconds
+                delay(1000);               // wait for a second
                 digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-                delay(2000);               // wait for 2 seconds
-		//Serial.print("This just in ... ");
-		//Serial.println(receivedChars);
-		newData = false;
+                delay(1000);               // wait for a second
 	}
 }
